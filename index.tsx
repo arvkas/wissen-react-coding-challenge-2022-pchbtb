8fbfaa9cf38d7446b9a6c39df41e5cd96da285ca
arvkas <arvkas@gmail.com>
2022-08-29 23:43:49 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { useState } from 'react';
+import { useEffect } from 'react';
 import { render } from 'react-dom';
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
@@ -100,9 +101,35 @@ const App = () => {
                 Next
               </Button>
             </Form>
+            <Toast />
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+const Toast = (props) => {
+  const { toastList, position } = props;
+  const [list, setList] = useState(toastList);
+
+  useEffect(() => {
+    setList(toastList);
+  }, [toastList, list]);
+
+  return (
+    <div className={`notification-container ${position}`}>
+      {list.map((toast, i) => (
+        <div key={i} className={`notification toast ${position}`}>
+          <button>X</button>
+          <div className="notification-image">
+            <img src={toast.icon} alt="" />
+          </div>
+          <div>
+            <p className="notification-title">{toast.title}</p>
+            <p className="notification-message">{toast.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
