0026b6210cb3693a220d8fb400b2bc95cd67c1af
arvkas <arvkas@gmail.com>
2022-08-29 17:08:57 +0530

    1 file modified

--- index.tsx
+++ index.tsx
@@ -12,7 +12,7 @@ const App = () => {
           <div>
             <form>
               <div>
-                <label>Username/Email</label>
+                <label>Username/Email:</label>
                 <input type="text" />
                 <div></div>
               </div>
